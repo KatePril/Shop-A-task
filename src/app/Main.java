5741aea12e37cc1a52b219e7aee242cf189b5941
KatePril <ket14pril14@gmail.com>
2023-06-06 23:19:53 +0300

    Fix main

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,11 +2,7 @@ package app;
 
 import app.controllers.AppController;
 
-import java.util.Scanner;
-
 public class Main {
-
-    static Scanner scanner;
     public  static void main(String[] args) {
         AppController appController = new AppController();
         appController.runApp();
