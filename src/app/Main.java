4b580b8deb8e5e578871c131e9b57472076cfb50
KatePril <ket14pril14@gmail.com>
2023-06-06 22:49:51 +0300

    Add close option

--- src/app/Main.java
+++ src/app/Main.java
@@ -17,6 +17,7 @@ public class Main {
                 Choose an option:
                 1 - order without delivery
                 2 - order with delivery
+                0 - close app
                 """);
         scanner = new Scanner(System.in);
         return scanner.nextInt();
@@ -32,6 +33,7 @@ public class Main {
                 OrderBController controller = new OrderBController();
                 controller.handleData();
             }
+            case 0 -> System.out.println("App closed");
             default -> {
                 System.out.println("No such option :-(");
                 filterOption(getOption());
